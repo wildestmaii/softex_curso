62a12ca4c6f008c98cc9edcf10dd0d1be925689f
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2023-08-10 17:32:11 +0000

    exemplos da aula passada pois perdi o antigo

---
+++ aula08_10.js
@@ -0,0 +1,22 @@
+function exemplo(){
+    console.log(teste);
+  }
+  
+  exemplo();
+  
+  
+  nome = Valdete;
+  
+  function exibirSaudacao() {
+    const mensagem = `Olá + ${nome}!`;
+    console.log(mensagem);
+  };
+  
+  
+  dados = [Maiara, 22, 27, 9, 2000];
+  
+  function cadastro() {
+    const dados = [];
+  console.log(`${nome}, cadastro realizado com sucesso!`);
+  console.log(`Seus dados são: Nome: ${nome}, Idade:${idade}, Dia de nascimento: ${dia}, Mês do nascimento: ${mes}, Ano do nascimento: ${ano}`)
+    };
