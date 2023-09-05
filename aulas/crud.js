60f9af2e419324507aabd861f4b917245e4069b7
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2023-09-05 18:43:08 +0000

    arrumei objeto aluno

--- aulas/crud.js
+++ aulas/crud.js
@@ -5,25 +5,25 @@ const readline = require('readline-sync');
 //while (loop){
 
 
-    const aluno1 = {
-        matricula: 0,
-        nomes: 'Maiara',
-        notaa: [5, 7, 10, 9]
-    };
-    const aluno2 = {
-        matricula: 1,
-        nomes: 'Evelyn',
-        notas: [3, 6, 6, 9]
-    };
+    const alunos = [
+        {id: 0,
+        nome: 'Maiara',
+        notas: [5, 7, 10, 9]
+        },
+        {id: 1,
+        nome: 'Evelyn',
+        notas: [2, 3, 0, 10]
+        }
+    ];
 
 
     console.log("~~~~~~~~MENU DE CADASTROS~~~~~~~~ \n 1 - Listar Alunos \n 2 - Cadastrar Aluno \n 3 - Atualizar Aluno \n 4 - Deletar Aluno \n 5 - Sair do Sistema");
     const menu = readline.question("Escolha uma das opções e digite o número: ");
-
+//pesquisar aluno tb    console.log(`Nome: ${alunos.nome} \n Notas: ${alunos.notas} \n Média: ${media}`);
     
     switch (menu) {
         case 1:
-            console.log(aluno1, aluno2);
+            console.log();
             break;
         case 2:
             //console.log();
