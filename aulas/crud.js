3adb750254e988fec3654c56cda1b8980de0ff06
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2023-09-05 19:39:46 +0000

    vou me matar as 23:99

--- aulas/crud.js
+++ aulas/crud.js
@@ -17,10 +17,19 @@ const readline = require('readline-sync');
     console.clear();
     console.log("~~~~~~~~MENU DE CADASTROS~~~~~~~~ \n 0 - Sair do Sistema \n 1 - Listar Alunos \n 2 -Cadastrar Aluno \n 3 - Buscar Aluno \n 4 - Alterar Aluno \n 5 - Remover Aluno");
     const menu = readline.question("Escolha uma das opções e digite o número: ");
-//pesquisar aluno tb    console.log(`Nome: ${alunos.nome} \n Notas: ${alunos.notas} \n Média: ${media}`);
+
     switch (menu) {
         case 1:
-            console.log();
+            console.log("Lista dos alunos: ")
+            for (const aluno of alunos) { 
+                function calcularMedia(notas) {
+                    const soma = notas.reduce((a, b) => a + b, 0);
+                    const media = soma / notas.length;
+                    return media;
+                }
+                const media = calcularMedia(aluno.notas);
+            };
+            console.log(`Matricula: ${a.matricula} \n Nome: ${a.nome} \n Média : ${media}`);
             break;
         case 2:
             console.log();
@@ -34,6 +43,24 @@ const readline = require('readline-sync');
 //}
 
 
+/*cópia do prof case 1
+console.log("listando alunos");
+
+let soma = 0;
+
+console.log(`Matrícula: ${a.matricula}`);
+//[...]
+
+for (let i = 0; i < a.notas.length; i++) {
+    console.log(`\t Nota ${i + 1}: ${a.notas.[i]}`);
+}
+
+let media = soma / a.notas.length;
+console.log(`\t Média: ${media}`);
+readline.KeyInPause();
+break;
+
+*/
 
 
 
