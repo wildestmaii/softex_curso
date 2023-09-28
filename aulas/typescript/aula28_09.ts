db980fdf3e3c618725193298b8331f05fdf22f91
Maiara Meneses <52250674+wildestmaii@users.noreply.github.com>
2023-09-28 19:24:11 +0000

    smt e arruma as pastas

---
+++ aulas/typescript/aula28_09.ts
@@ -0,0 +1,61 @@
+//import { ConnectionAcquireTimeoutError } from "sequelize";
+//o package só tinha readline-sync e typescript
+
+
+const alo:number = 7;
+
+Math.abs(alo);
+
+console.log(alo);
+
+let numero: number = 10;
+let txt: string = "e tu vai morder, é?";
+let logica:boolean = false;
+let nula: null = null;
+let arr: Array<any> = [1, 2, 3, 4, 5, 6];
+let ray: Array<string> = new Array("1", "2", "3", "4", "5", "6", "2");
+
+
+arr.push(5);
+//ray.push("e tu vai morder, é?");
+
+let valor:number = 5;
+let valorTxt: string = valor.toFixed(2);
+valor = parseInt(valorTxt);
+
+//let valorMax:number = valor;
+
+let letras = ["a", "b", "c"];
+var teste = ray.concat(letras);
+console.log(teste);
+
+var letra: string = txt.charAt(17);
+console.log(letra);
+
+var remove: string = txt.trim();
+console.log(remove);
+
+var qual = ray.indexOf("2");
+console.log(qual);
+
+var onde = ray.lastIndexOf("2");
+console.log(onde);
+
+let frase = ["e", "tu", "vai", "morder", ",", "é", "?"];
+var um = frase.slice(3);
+console.log(um);
+
+let pi:number = (3.1486756);
+var piString = pi.toString();
+console.log(piString);
+
+console.log(txt.substring(0, 3));
+
+console.log(txt.toUpperCase());
+
+let aaaa: string = "E TU VAI MORDER, É?";
+console.log(aaaa.toLowerCase());
+
+
+
+
